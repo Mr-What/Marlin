5e69c54e06f124e8d0f8fea7ac5536e71559be86
Aaron Mr-What <aaron@boim.com>
2014-10-17 09:07:06 -0600

    clean up and matrixify

--- util/cart2delta.m
+++ util/cart2delta.m
@@ -3,7 +3,7 @@
 %
 %      +Y                       3(RAMPS-Z)
 %       ^                          X
-%       |  Card coords            / \          Tower name/number
+%       |  Cart coords            / \          Tower name/number
 %       |                        /   \
 %       +-->+X       (RAMPS-X)1 +-----+ 2 (RAMPS-Y)
 %
@@ -12,6 +12,8 @@
 %       RADIUS   -- Marlin DELTA_RADIUS, which is radius from tip to center
 %                   of tower pivot for diagonal arm, minus effector offset
 %                   (kind of a radius - effector_offset)
+%       radius(3)-- instead of one RADIUS for all towers,
+%                   provide individual radius for each tower 
 %       RodLen   -- length between center of pivots on diagonal rods
 function delta = cart2delta(DeltaParams,cart)
 
@@ -22,24 +24,13 @@ else
 end
 s = 0.8660254037844386; % sind(60)
 c = 0.5;                % cosd(60)
-%T1x = -s * radius(1);
-%T1y = -c * radius(1);
-%T2x =  s * radius(2);
-%T2y = -c * radius(2);
-%T3x = 0;
-%T3y = radius(3);
-tp = [[-s,-c]*radius(1);...
-      [ s,-c]*radius(2);...
-      0,radius(3)];  % tower positions
-
 r2 = DeltaParams.RodLen * DeltaParams.RodLen;
-%dx1 = T1x - cart(1);
-%dy1 = T1y - cart(2);
-%dx2 = T2x - cart(1);
-%dy2 = T2y - cart(2);
-%dx3 = T3x - cart(1);
-%dy3 = T3y - cart(2);
+
+tp = [[-s,-c]*radius(1);...         % tower positions
+      [ s,-c]*radius(2);...
+      0,radius(3)];
+
 d = tp - [1;1;1] * cart(1:2);  % offset from tower XY to cart XY
-d2 = sum(d .* d,2)';  % square of this distance
-delta = sqrt(r2 - d2) + cart(3);
+d = sum(d .* d,2)';  % square of this distance
+delta = sqrt(r2 - d) + cart(3);
 end
